855baf03b528145dc93874aabd83cc14029f5c35
LIKANG <79990647+SAKURA-CAT@users.noreply.github.com>
2022-10-05 15:50:07 +0800

    完成了完成了商品列表的开发

---
+++ components/my-goods-list/my-goods-list.vue
@@ -0,0 +1,75 @@
+<template>
+  <view>
+    <view class="goods-item">
+      <!-- 商品左侧的图片区域 -->
+      <view class="goods-item-left">
+        <!-- 为了防止有一些图片不存在，使用一张默认的空图片 -->
+        <image :src="goods.goods_small_logo || defaultPic" class="goods-left-pic" mode="widthFix"></image>
+      </view>
+      <!-- 商品的右侧信息区域 -->
+      <view class="goods-item-right">
+        <!-- 商品标题 -->
+        <view class="goods-right-name">{{goods.goods_name}}</view>
+        <view class="goods-right-info-box">
+          <!-- 商品价格 -->
+          <view class="goods-right-price">￥{{goods.goods_price | tofixed}}</view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-goods-list",
+    props: {
+      // 商品的信息对象
+      goods: {
+        type: Object,
+        defaul: {},
+      },
+    },
+    data() {
+      return {
+        // 默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+      }
+    },
+    methods:{
+
+    },
+    filters: {
+      // 把数字处理为带两位小数点的数字
+      tofixed(num) {
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.goods-item{
+  display: flex;
+  padding: 20rpx 20rpx;
+  border-bottom: 1px solid #f0f0f0;
+  .goods-item-left{
+    margin-right: 20rpx;
+    .goods-left-pic{
+      width: 200rpx;
+      display: block;
+    }
+  }
+  .goods-item-right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-right-name{
+      font-size: 28rpx;
+    }
+    .goods-right-price{
+      font-size: 36rpx;
+      color: #c00000;
+    }
+  }
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,20 @@
 <template>
   <view>
-    
+    <view class="goods-list">
+      <!-- 如果列表存在 -->
+      <block v-if="goodsList.length!==0">
+        <view v-for="(item, i) in goodsList" :key="i" @click="gotoGoodsDetail(item)">
+          <my-goods-list :goods="item"></my-goods-list>
+        </view>
+      </block>
+      <!-- 不存在的话就给个不存在的提示 -->
+      <block v-else>
+        <view class="goods-not-exists">
+          <uni-icons color="#dddddd" type="cart" size="100"></uni-icons>
+          <text>Oh,该类别暂时没有商品哦～</text>
+        </view>
+      </block>
+    </view>
   </view>
 </template>
 
@@ -8,12 +22,83 @@
   export default {
     data() {
       return {
-        
+        // 请求参数对象
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        total:0,
+        goodsList: [],
+        // 默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+        // 下拉触底的节流阀，判断是否正在请求数据
+        isloading: false
       };
+    },
+    onLoad(options){
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    methods: {
+      async getGoodsList(cb){
+        this.isloading = true
+        const {data: resp} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        // console.log(resp)
+        // 只要数据请求完毕，就立即按需调用 cb 回调函数
+        cb && cb()
+        if (resp.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+        this.goodsList = [...this.goodsList, ...resp.message.goods]
+        this.total = resp.message.total
+        this.isloading = false
+      },
+      gotoGoodsDetail(item){
+        uni.navigateTo({
+          url:"/subpkg/goods_detail/goods_detail?goods_id=" + item.goods_id
+        })
+      }
+    },
+    // 触底的事件
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg({title: "没有更多信息了"})
+      if (this.isloading) return
+      // 让页码值自增 +1
+      this.queryObj.pagenum += 1
+      // 重新获取列表数据
+      this.getGoodsList()
+    },
+    // 下拉刷新的事件
+    onPullDownRefresh() {
+      // 1. 重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+      // 2. 重新发起请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
     }
   }
 </script>
 
 <style lang="scss">
-
+.goods-list{
+  .goods-not-exists{
+    width: 400rpx;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -60%);
+    text{
+      font-size: 30rpx;
+      color: #dddddd;
+    }
+  }
+}
 </style>
